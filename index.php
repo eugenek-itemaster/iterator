270d28bf126054d7b90d1907096af8a08c63c8a2
eugenek-itemaster <eugene.k@a-team.global>
2022-12-02 23:52:19 +0200

    Revert "PTN PNH"
    This reverts commit 0f262a34f0a662fa8417588c9662ac7270052552.

--- index.php
+++ index.php
@@ -6,6 +6,4 @@ foreach ($iterator as $value) {
     echo "current - " . $value . PHP_EOL;
     echo "previous - " . $iterator->prev() . PHP_EOL;
     echo "---------" . PHP_EOL;
-
-    echo "PTNPNH";
 }
